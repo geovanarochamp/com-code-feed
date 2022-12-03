2f13245e20a930d1735c53415448ec25ef4c4fbe
geovanarochamp <geovana.rocha.martins@gmail.com>
2022-12-03 15:49:07 -0300

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+import comcodeLogo from '../assets/favicon.png'
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={comcodeLogo} alt="logo da comunidade code" />
+            <h1>ComCode Feed</h1>
+        </header>
+    )
+}
