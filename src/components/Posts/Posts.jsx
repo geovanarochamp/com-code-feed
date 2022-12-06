84a9ad94bd205c2549138f9d8e732e401f31118f
geovanarochamp <geovana.rocha.martins@gmail.com>
2022-12-06 16:39:15 -0300

    adjust styles of post component

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,7 +2,35 @@ import styles from './Posts.module.css'
 
 export function Post() {
     return (
-        <div className={styles.post}>
+        <div className="posts">
+            <div className={styles.post}>
+                <div className={styles.postHeader}>
+                    <div className={styles.author}>
+                        <div className={styles.authorImg}>
+                            <img src="https://github.com/geovanarochamp.png" />
+                        </div>
+                        <div className={styles.authorInfo}>
+                            <strong>Geovana Rocha</strong>
+                            <span>Dev Front-End</span>
+                        </div>
+                    </div>
+                    <time title='06 de Dezembro de 2022' dateTime='06-12-2022 08:30:34'>Publicado há 1h</time>
+                </div>
+
+                <div className={styles.postContent}>
+                    <p>Fala galeraa 👋</p>
+
+                    <p>Contruí um sistema de busca de usuários do github e foi minha primeira aplicação react. 🚀</p>
+
+                    <p>Quem puder ir dar uma olhada, e adicionar o user de vocês para ver se está tudo ok!</p>
+
+                    <p><a href="#">👉 github.com/geovanarochamp/search-github-users</a></p>
+
+                    <p><a href="#">#novoprojeto</a> <a href="#">#reactjs</a> <a href="#">#javascript</a></p>
+                </div>
+            </div>
+
+            <div className={styles.post}>
             <div className={styles.postHeader}>
                 <div className={styles.author}>
                     <div className={styles.authorImg}>
@@ -25,8 +53,9 @@ export function Post() {
 
                 <p><a href="#">👉 github.com/geovanarochamp/search-github-users</a></p>
 
-                <p><a href="#">#novoprojeto</a> <a href="#">#reactjs</a> <a href="#">#javascript</a></p>
-            </div>
+            <p><a href="#">#novoprojeto</a> <a href="#">#reactjs</a> <a href="#">#javascript</a></p>
+        </div>
+        </div>
         </div>
     )
 }
